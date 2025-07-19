e5235b26b2198d5084bb5b4801566aef20536499
AGU GODWIN CHIBUIKE <perryfresh25@gmail.com>
2025-07-19 17:47:00 +0100

    Enable static folder for images

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 // Load your JSON file as data
 const foodData = require('./work.json');
 
+app.use(express.static('images'));
+
 // Define your route
 app.get('/foods', function(req, res) {
   res.json(foodData); // Serve the contents of your JSON file
